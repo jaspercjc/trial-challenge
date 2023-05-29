a0d6e92d80dd0f1859d627f7c885aa1f95cc7352
Jasper Castillo <jaspercjcastillo@gmail.com>
2023-05-30 04:33:19 +0800

    implement FormComponent to register page

--- resources/js/pages/Register.vue
+++ resources/js/pages/Register.vue
@@ -1,29 +1,20 @@
 <template>
-    <div class="w-screen sm:max-w-md mx-auto bg-white rounded py-10 px-4">
-        <h1 class="text-3xl font-bold w-full text-center">Register</h1>
-        <div class="mt-8">
-            <form class="flex flex-col gap-4">
-                <InputComponent label="Username" required />
-                <InputComponent label="Email Address" required type="email" />
-                <InputComponent label="Phone Number" required />
-                <InputComponent type="password" label="Password" required />
-                <InputComponent
-                    type="password"
-                    label="Confirm Password"
-                    required
-                />
-                <div class="flex justify-between items-center mt-4">
-                    <router-link to="login" class="text-blue-500">
-                        Login
-                    </router-link>
-                    <PrimaryButton>Register</PrimaryButton>
-                </div>
-            </form>
-        </div>
-    </div>
+    <FormComponent title="Register">
+        <InputComponent label="Username" required />
+        <InputComponent label="Email Address" required type="email" />
+        <InputComponent label="Phone Number" required />
+        <InputComponent type="password" label="Password" required />
+        <InputComponent type="password" label="Confirm Password" required />
+
+        <template #actions>
+            <router-link to="login" class="text-blue-500"> Login </router-link>
+            <PrimaryButton>Register</PrimaryButton>
+        </template>
+    </FormComponent>
 </template>
 
 <script setup>
+import FormComponent from "../components/FormComponent.vue";
 import InputComponent from "../components/InputComponent.vue";
 import PrimaryButton from "../components/PrimaryButton.vue";
 </script>
